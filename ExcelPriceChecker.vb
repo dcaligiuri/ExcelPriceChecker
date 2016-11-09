0dd2dc01fd8b61840fc599bc2788664baa3e96ba
dcaligiuri <daniel.caligiuri44@gmail.com>
2016-11-09 01:49:54 -0500

    Squashed some bugs

--- ExcelPriceChecker.vb
+++ ExcelPriceChecker.vb
@@ -1,8 +1,15 @@
-Sub GetPrice()
+Private Sub Worksheet_Change(ByVal Target As Range)
+    Dim KeyCells As Range
+    Set KeyCells = Range("A1")
+    If Not Application.Intersect(KeyCells, Range(Target.Address)) _
+           Is Nothing Then
+       
     Dim sku As String
     sku = Range("A1").Value
     If Len(sku) = 5 Then
         sku = "0" + sku
+    ElseIf Len(sku) = 0 Then
+        sku = 123456
     ElseIf Left(sku, 1) = "x" Then
         sku = Mid(sku, 2, Len(sku))
     End If
@@ -10,17 +17,20 @@ Sub GetPrice()
     sURL = "http://www.microcenter.com/search/search_results.aspx?Ntt=" & sku
     sResult = GetHTTPResult(sURL)
     Dim unparsedPrice As String, i As Long, j As Long
-    i = InStr(sResult, "$</span>")
-    j = InStrRev(sResult, "</span>")
-    unparsedPrice = Mid(sResult, i + 1, j - i - 1)
+    If InStr(sResult, "We're sorry") Then
+        unparsedPrice = 0
+    Else
+        i = InStr(sResult, "$</span>")
+        j = InStrRev(sResult, "</span>")
+        unparsedPrice = Mid(sResult, i + 1, j - i - 1)
+    End If
     Dim price As String
     price = onlyDigits(unparsedPrice)
     Range("F1") = price
+    End If
 End Sub
-
-
-
-
+        
+ 
 Function GetHTTPResult(sURL As String) As String
     Dim XMLHTTP As Variant, sResult As String
     Set XMLHTTP = CreateObject("WinHttp.WinHttpRequest.5.1")
@@ -35,16 +45,42 @@ End Function
 
 
 Function onlyDigits(s As String) As String
-    Dim retval As String    
-    Dim i As Integer       
-    retval = ""                         
+    Dim retval As String
+    Dim i As Integer
+    retval = ""
     For i = 1 To Len(s)
         If Mid(s, i, 1) >= "0" And Mid(s, i, 1) <= "9" Or Mid(s, i, 1) = "." Then
             retval = retval + Mid(s, i, 1)
         End If
-    Next                  
+    Next
     onlyDigits = retval
 End Function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
